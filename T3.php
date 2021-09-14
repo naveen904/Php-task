04597b3a31bd7206c4cd5bca278de4e8cf80a47a
Naveen <naveentanwar120@gmail.com>
2021-09-14 12:25:17 -0700

    first commit

---
+++ T3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reverse</title>
+</head>
+<body>
+<?php
+function Reverse($s){
+	return strrev($s);
+}
+$s = "Rex web";
+echo Reverse($s)
+?>
+
+</body>
+</html>
